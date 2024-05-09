6fc7bd6cc4bb0feba35300059b378d8226eb670c
Ankur Kushwaha <akushwaha@adobe.com>
2024-05-09 19:36:56 +0530

    url updated

--- config/env/production.js
+++ config/env/production.js
@@ -6,6 +6,6 @@ module.exports = {
     port: process.env.PORT || 3000,
     mongo: {
         uri: process.env.MONGOLAB_URI ||
-            'mongodb://127.0.0.1:27017/pisignage-server-dev'
+            'mongodb+srv://ankur:ankur@cluster0.wgb6k.mongodb.net/pisignage-server?retryWrites=true&w=majority&appName=Cluster0'
     }
 };
